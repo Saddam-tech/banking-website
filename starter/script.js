5be3420816367b88b0cbcb2b6ba43eb3ba5a8044
Saddam-tech <salokhiddinov0727@gmail.com>
2021-02-19 10:54:43 +0900

    some more changes

--- starter/script.js
+++ starter/script.js
@@ -172,3 +172,27 @@ console.log(document.querySelector('h1').querySelectorAll('.highlight'));
 // [...h1.parentElement.children].forEach(el => {
 //   if (el !== h1) el.style.transform = 'scale(0.2)';
 // });
+
+/// I HAVE TO REVIEW THIS CODE ONCE MORE ////////
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  //Guard clause
+  if (!clicked) return;
+
+  //Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
