52fbf3d8da09851937f4933e7bad9c0cc6b21789
Saddam-tech <salokhiddinov0727@gmail.com>
2021-02-19 09:20:26 +0900

    a lot of chages

---
+++ starter/script.js
@@ -0,0 +1,174 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+/////// Scrolling
+
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
+
+// btnScrollTo.addEventListener('click', function (e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   console.log('s1coords', s1coords);
+//   console.log('Current Scroll (X/Y)', pageXOffset, pageYOffset);
+
+/////// Scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log('s1coords', s1coords);
+  console.log('Current Scroll (X/Y)', pageXOffset, pageYOffset);
+
+  // Scrolling;
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(el => {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     console.log('LINK');
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(e.target);
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    console.log('LINK');
+  }
+});
+
+console.log(document.querySelector('h1').querySelectorAll('.highlight'));
+
+////////////////////////////
+//Lectures
+
+// const header = document.querySelector('.header');
+
+// Creating and inserting elements
+
+// const message = document.createElement('div');
+
+// message.classList.add('cookie-message');
+
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.append(message);
+
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
+
+// message.style.width = '120%';
+// message.style.backgroundColor = '#000';
+
+// console.log((message.style.width = '120%'));
+
+// console.log(Number.parseFloat('43.2342'));
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+/////// Scrolling
+
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
+
+// btnScrollTo.addEventListener('click', function (e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   console.log('s1coords', s1coords);
+//   console.log('Current Scroll (X/Y)', pageXOffset, pageYOffset);
+
+//Scrolling
+// window.scrollTo(
+//   s1coords.left + window.pageXOffset,
+//   s1coords.top + window.pageYOffset
+// );
+
+// window.scrollTo({
+//   left: s1coords.left + window.pageXOffset,
+//   top: s1coords.top + window.pageYOffset,
+//   behavior: 'smooth',
+// });
+
+//   section1.scrollIntoView({ behavior: 'smooth' });
+// });
+
+// const h1 = document.querySelector('h1');
+
+// const alertH1 = function (e) {
+//   alert('AddEventListener: Great! You are reading the heading!');
+//   h1.removeEventListener('mouseenter', alertH1);
+// };
+
+// h1.addEventListener('mouseenter', alertH1);
+
+// const randomInt = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+
+// const randomColor = () => {
+//   return `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+// };
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('Link', e.target);
+// });
+
+// const h1 = document.querySelector('h1');
+
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(el => {
+//   if (el !== h1) el.style.transform = 'scale(0.2)';
+// });
